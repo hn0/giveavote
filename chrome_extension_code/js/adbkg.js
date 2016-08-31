e695769074749366eb002d1c7a5896de81a5202b
Hrvoje Novosel <hnovosel@live.com>
2016-08-31 18:34:58 +0200

    + background page for chrome.webrequest event

--- chrome_extension_code/js/adbkg.js
+++ chrome_extension_code/js/adbkg.js
@@ -2,7 +2,15 @@
 	
 	var adbkg = function(){
 
-		console.log('here we go!')
+
+		// ok, now we are talking at least we have a proper event listener!
+		// chrome.webRequest.onBeforeSendHeaders.addListener(function() {
+		// 	console.log('web request overiden!!!');
+
+		// 	return false;
+		// }, 
+		// {urls: [ "<all_urls>" ]},
+		// ['requestHeaders','blocking']);
 
 	};
 
