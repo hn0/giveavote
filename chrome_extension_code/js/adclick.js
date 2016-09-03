bd6926ce82b3ac3ee1f0a2f25f1d638d3b0e14ac
Hrvoje Novosel <hnovosel@live.com>
2016-09-03 08:52:16 +0200

    * content script refractoring

--- chrome_extension_code/js/adclick.js
+++ chrome_extension_code/js/adclick.js
@@ -3,86 +3,77 @@
 	var adclick = function()
 	{
 
-		// Process headers using messaging sys
-		chrome.runtime.onMessage.addListener(function(msg, sender, callback) {
-			// console.log('msg >>>', msg)
 
-			// Ok, msg is beeing transmitted but how should I respond to it!
-		});
-
-		document.addEventListener('DOMContentLoaded', 
-			function(){
-
-				var click_it = function(url, data)
-				{
-
-					// TODO: this is not brigthest idea, you should return to xhr request
-					var perform_request = function(timeout) {
-
-						//TODO: check which url to use
-						// console.log('visiting url:' , url);
-						// console.log('visiting data:', data);
+		var Constructor = function ()
+		{
+			document.addEventListener('DOMContentLoaded', this.search_links.bind(this), false);
+			chrome.runtime.onMessage.addListener(this.process_headers);
+		}
 
 
-						// ok, for now single request is enough! stop playing with it!?
-						var xhr = new XMLHttpRequest();
-
-						// xhr.addEventListener('abort', function() {
-						// 	if( xhr.status === 0 ){
-						// 		// setTimeout(perform_request, timeout, timeout);
-						// 		// change id of the url first
-						// 	}
-						// });
+		// Process request headers, 302 issue with redirection to http protocol
+		// still not sure how to  handle this
+		Constructor.prototype.process_headers = function(msg, sender, callback) 
+		{
+			// console.log('Headers retrieval', msg, this);
+		}
 
 
-						var domain = "test";
+		Constructor.prototype.search_links = function()
+		{
+		
+			// This is temporary solution, domain needs to be extracted, and only adwords cliked?
+			// There is also an issue with match pattern, (can found origin page as well)
+			var re = /adurl=.+\.hr/;
 
-						// setup header intercept message
-						chrome.runtime.sendMessage({
-							"domain"           : domain
-						}, 
-						function(resp) {
-							// console.log('!! response from orig', resp.msg)
+			var a  = document.getElementsByTagName('a');
+			for(var i=0; i < a.length; a++){
+				var id = a[i].getAttribute('id');
+				var	link = a[i].getAttribute('href');
+				var data = a[i].getAttribute('data-original-click-url');
 
-							if(resp.status){
-								// console.log("Opening request for: " + url);
-
-								xhr.open('GET', url, true);
-								xhr.send();
-							}
-
-						});
-
-					}
-
-					var timeouts = [750];
-					timeouts.forEach(function(x) {
-						// console.log(x)
-						setTimeout(perform_request, x, x);
-					});
-
-				};
-
-				var a  = document.getElementsByTagName('a');
-				// There is also an issue with match pattern, (can found origin page as well)
-				var re = /adurl=.+\.hr/;
-
-				for(var i=0; i < a.length; a++){
-					var id = a[i].getAttribute('id');
-					var	link = a[i].getAttribute('href');
-					var data = a[i].getAttribute('data-original-click-url');
-
-					if( id && id.startsWith('aw') || data){
-						// adwords flag?
-						// TODO: limit to single web page
-						if( link.match(re) ){
-							click_it(link, data);
-						}
+				if( id && id.startsWith('aw') || data){
+					if( link.match(re) ){
+						this.request(link, data);
 					}
 				}
+			}
 
-			}, false);
+		}
 
+
+		Constructor.prototype.request = function(url, alt_url)
+		{
+			// TODO: here would go the code for url of choice
+			var timeouts = [250];
+			timeouts.forEach(function(x) {
+				setTimeout(this.new_request, x, url);
+			}.bind(this));
+		}
+
+
+		Constructor.prototype.new_request = function(url, time_out)
+		{
+			console.log('visiting: ' + url);
+			var xhr = new XMLHttpRequest();
+
+			var domain = 'test';
+			chrome.runtime.sendMessage({
+				'domain': domain
+			},
+			function(resp) {
+
+				if(resp.status){
+					xhr.open('GET', url);
+					xhr.send();
+				}
+
+				console.log('Background response:', resp);
+			});
+		}
+
+
+		return new Constructor();
 	};
 
 	adclick();
